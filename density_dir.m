bf5b5be53c72dd9975814132e4b02b360f6541fb
Bill Pezoulas <bPezoulas@gmail.com>
2015-12-05 16:04:05 +0200

    Create density_dir.m

---
+++ density_dir.m
@@ -0,0 +1,26 @@
+function [kden,N,K] = density_dir(a)
+%DENSITY        Density
+%
+%   kden = density_dir(CIJ);
+%   [kden,N,K] = density_dir(CIJ);
+%
+%   Density is the fraction of present connections to possible connections.
+%
+%   Input:      a,      I/O object handler
+%
+%   Output:     kden,   density
+%               N,      number of vertices
+%               K,      number of edges
+%
+%   Notes:  Assumes CIJ is directed and has no self-connections.
+%           Weight information is discarded.
+%
+%
+%   Olaf Sporns, Indiana University, 2002/2007/2008
+
+% Modification history:
+% 2015: CIJ is directly accessed without being loaded into memory
+
+N = size(a.CIJ, 1);
+K = nnz(a.CIJ);
+kden = K/(N^2-N);
