89e4a61a3ce02d07b0a996cc30e704ea46c0508d
vpz4 <bPezoulas@gmail.com>
2015-12-05 15:33:24 +0200

    Update read_matfile.m
    Update: faster execution, comments added.

--- read_matfile.m
+++ read_matfile.m
@@ -1,10 +1,23 @@
-function CIJ=read_matfile(path)
-a=matfile(path);
-fiel=fields(a);
-for i=1:length(fiel)
-    [m n]=size(getfield(a,fiel{i}))
-    if(m==n)
-        CIJ=fiel{i}
-    end;
+function [a, CIJ] = read_matfile(path)
 
-end;
+%Opens IO connection with the disk in order to retrieve (big) data without 
+%loading them on workspace.
+%
+%Input:      path,   full path specifying the location of the directed 
+%                    (binary/weighted) connection matrix
+%
+%Output:     a,     IO handler
+%            CIJ,   string that contains the name of the connection matrix
+%
+%Notes:     Full path should contain only one .mat file with the desired 
+%           connection matrix.
+%
+
+a = matfile(path);
+b = whos(a);
+
+if(min(b.size) == max(b.size))
+    CIJ = b.name;
+else
+    error('Matrix should be square!');
+end
