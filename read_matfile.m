6dc29fe9f86fd46e7773d6f29ea08f6787bbdbc4
Bill Pezoulas <bPezoulas@gmail.com>
2015-12-05 15:51:04 +0200

    Update read_matfile.m
    Update: Faster execution, comments added.

--- read_matfile.m
+++ read_matfile.m
@@ -1,16 +1,16 @@
 function [a, CIJ] = read_matfile(path)
 
-%Opens IO connection with the disk in order to retrieve (big) data without 
-%loading them on workspace.
+%   Opens I/O connection with the disk in order to retrieve (big) data 
+%   without loading them on workspace.
 %
-%Input:      path,   full path specifying the location of the directed 
-%                    (binary/weighted) connection matrix
+%   Input:      path,   full path specifying the location of the directed 
+%                       (binary/weighted) connection matrix
 %
-%Output:     a,     IO handler
-%            CIJ,   string that contains the name of the connection matrix
+%   Output:     a,     I/O object handler
+%               CIJ,   string containing the name of the connection matrix
 %
-%Notes:     Full path should contain only one .mat file with the desired 
-%           connection matrix.
+%   Notes:      Full path should contain one .mat file with the desired 
+%               connection matrix only.
 %
 
 a = matfile(path);
