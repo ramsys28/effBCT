2dff1203cf91244520bcd24b460529bb53333b37
Bill Pezoulas <bPezoulas@gmail.com>
2016-01-18 00:24:39 +0200

    Create clustering_coef_wu.m

---
+++ clustering_coef_wu.m
@@ -0,0 +1,25 @@
+function C=clustering_coef_wu(a)
+%CLUSTERING_COEF_WU     Clustering coefficient
+%
+%   C = clustering_coef_wu(W);
+%
+%   The weighted clustering coefficient is the average "intensity" of 
+%   triangles around a node.
+%
+%   Input:      a,      I/O object handler
+%
+%   Output:     C,      clustering coefficient vector
+%
+%   Reference: Onnela et al. (2005) Phys Rev E 71:065103
+%
+%
+%   Mika Rubinov, UNSW, 2007-2010
+
+% Modification history:
+% 2016: CIJ is directly accessed without being loaded into memory
+
+K=sum(a.CIJ~=0,2); 
+% cyc3 = diag((nthroot((a.CIJ),3))^3);
+cyc3=diag(((a.CIJ).^(1/3))^3);           
+K(cyc3==0)=inf;             %if no 3-cycles exist, make C=0 (via K=inf)
+C=cyc3./(K.*(K-1));         %clustering coefficient
