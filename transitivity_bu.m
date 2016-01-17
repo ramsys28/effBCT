ca573d968a2208c74045d840dc90c7a685ec8152
Bill Pezoulas <bPezoulas@gmail.com>
2016-01-18 01:45:44 +0200

    Create transitivity_bu.m

---
+++ transitivity_bu.m
@@ -0,0 +1,23 @@
+function [C_tri] = transitivity_bu(a)
+%TRANSITIVITY_BU    Transitivity
+%
+%   T = transitivity_bu(a);
+%
+%   Transitivity is the ratio of 'triangles to triplets' in the network.
+%   (A classical version of the clustering coefficient).
+%
+%   Input:      a,      I/O object handler
+%
+%   Output:     T,      transitivity scalar
+%
+%   Reference: e.g. Humphries et al. (2008) Plos ONE 3: e0002051
+%
+%
+%   Alexandros Goulas, Maastricht University, 2010
+
+% Modification history:
+% 2016: CIJ is directly accessed without being loaded into memory
+
+C_tri = trace((a.CIJ)^3) / (sum(sum((a.CIJ)^2)) - trace((a.CIJ)^2));
+
+return;
