c65933ba2859d7072bd508a4c82b595329c91990
Bill Pezoulas <bPezoulas@gmail.com>
2016-03-07 01:24:04 +0200

    Update clustering_coef_wu.m

--- clustering_coef_wu.m
+++ clustering_coef_wu.m
@@ -10,6 +10,8 @@ function C = clustering_coef_wu(a)
 %
 %   Output:     C,      clustering coefficient vector
 %
+%   Notes:  CIJ is a weighted and undirected connection matrix.
+%
 %   Reference: Onnela et al. (2005) Phys Rev E 71:065103
 %
 %
@@ -19,7 +21,8 @@ function C = clustering_coef_wu(a)
 % 2016: CIJ is directly accessed without being loaded into memory
 
 K = sum((a.CIJ~=0),2); 
-% cyc3 = diag((nthroot((a.CIJ),3))^3);
+% cyc3 = diag((nthroot((a.CIJ),3))^3); - alternative
 cyc3 = diag(((a.CIJ).^(1/3))^3);           
 K(cyc3==0) = inf;             %if no 3-cycles exist, make C=0 (via K=inf)
 C = cyc3./(K.*(K-1));         %clustering coefficient
+
